36da115ae19018e161a33f4fb7488f1b2a45c3dd
Tiago Nunes <ntiagon@gmail.com>
2021-09-29 18:58:31 -0300

    sidebar Links and modal start

---
+++ src/components/ModalEditUser/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ModalEditUser() {
+  return <div></div>;
+}
+
+export default ModalEditUser;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logoWhite from '../../assets/logowhitefull.svg';
+import homeIcon from '../../assets/home.svg';
+import moneyIcon from '../../assets/money.svg';
+import clientsIcon from '../../assets/clients.svg';
 
 function Sidebar({ children }) {
   return (
-    <div>
+    <nav>
+      <img src={logoWhite} alt="Logo branco Cubos Academy" />
       <div>
-        <div>Home</div>
-        <div>Cobranças</div>
-        <div>Clientes</div>
+        <Link to="/home">
+          <img src={homeIcon} alt="ícone home" />
+          Home
+        </Link>
+        <Link to="/cobrancas">
+          <img src={moneyIcon} alt="ícone home" />
+          Cobranças
+        </Link>
+        <Link to="/clientes">
+          <img src={clientsIcon} alt="ícone home" />
+          Clientes
+        </Link>
         <button>Criar cobrança</button>
       </div>
       {children}
-    </div>
+    </nav>
   );
 }
 
