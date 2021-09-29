0db1c7be3230bcc7a44147abf0ce214ce4739a10
Tiago Nunes <ntiagon@gmail.com>
2021-09-29 18:16:24 -0300

    updated routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,18 +6,26 @@ import Register from '../pages/Register';
 import EditUser from '../pages/EditUser';
 import ClientRegister from '..//pages/ClientRegister';
 import AuthContext from '../contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Sidebar from '../components/Sidebar';
+import AddClient from '../pages/AddClient';
 
 function Routes() {
-  const [token, setToken] = useState()
+  const [token, setToken] = useState();
   return (
     <AuthContext.Provider value={{ token, setToken }}>
+      <ToastContainer />
       <Router>
         <Switch>
           <Route path="/" exact component={Login} />
           <Route path="/cadastro" component={Register} />
-          <Route path="/home" component={Home} />
           <Route path="/atualizar-usuario" component={EditUser} />
           <Route path="/cadastrar-cliente" component={ClientRegister} />
+          <Sidebar>
+            <Route path="/home" component={Home} />
+            <Route path="/adicionar-cliente" component={AddClient} />
+          </Sidebar>
         </Switch>
       </Router>
     </AuthContext.Provider>
