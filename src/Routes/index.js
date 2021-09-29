4b7f93ea0d441a5c2aded55af129957ff655ae6b
Karine Mendes <karinemendessantos@hotmail.com>
2021-09-29 00:34:56 -0300

    Páginas Login e Cadastro

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
+import Home from '../pages/Home';
+import Register from '../pages/Register';
+import EditUser from '../pages/EditUser';
+import ClientRegister from '..//pages/ClientRegister';
+import AuthContext from '../contexts/AuthContext';
 
 function Routes() {
+  const [token, setToken] = useState()
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>
-    </Router>
+    <AuthContext.Provider value={{ token, setToken }}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/cadastro" component={Register} />
+          <Route path="/home" component={Home} />
+          <Route path="/atualizar-usuario" component={EditUser} />
+          <Route path="/cadastrar-cliente" component={ClientRegister} />
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -5,25 +5,27 @@ import '../../styles/alignments.css';
 import '../../styles/form.css';
 import '../../styles/buttons.css';
 import CubosAcademyLogo from '../../assets/cubos-academy.svg';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-function Login() {
+function Register() {
   return (
     <div className="container-form">
-        <form className="form form-login items-center content-center">
+        <form className="form form-register">
           <img src={CubosAcademyLogo} alt="logo" />
           <div className="flex-column input">
+            <label>Nome</label>
+            <input id="user" type="text" placeholder="Novo Usuário" />
             <label>E-mail</label>
-            <input id="email" type="email" placeholder="exemplo@gmail.com" />
+            <input id="email" type="email" label="E-mail" placeholder="exemplo@gmail.com" />
             <label>Senha</label>
             <input id="password" type="password" label="Senha" placeholder="minhasenha" />
           </div>
             <button className="btn-pink-light flex-row items-center content-center">ENTRAR 
             </button>
         </form>
-        <h1>Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link></h1>
-    </div>
+        <h1>Já possui uma conta? <Link to="/">Acesse agora!</Link></h1>
+      </div>
   );
 }
 
-export default Login;
+export default Register;
