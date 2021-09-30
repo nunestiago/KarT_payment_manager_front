9dd368c90fb1a028c0d328394cda03a69b1eec2a
Tiago Nunes <ntiagon@gmail.com>
2021-09-30 15:48:59 -0300

    eye svg placement fix

--- src/components/PasswordInput/index.js
+++ src/components/PasswordInput/index.js
@@ -1,31 +1,33 @@
 import './style.scss';
-import '../../styles/form.scss'
+import '../../styles/form.scss';
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-function PasswordInput({ label, placeholder, value, setValue }){  
+function PasswordInput({ label, placeholder, value, setValue }) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="flex-column input">
       <label>{label}</label>
-      <input
-        id="password"
-        className="input-password"
-        type={showPassword ? 'text' : 'password'}
-        label="Senha"
-        placeholder={placeholder}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <FontAwesomeIcon
-        icon={showPassword ? faEye : faEyeSlash}
-        className="eye-password"
-        onClick={() => setShowPassword(!showPassword)}
-      />
+      <div className="input-password">
+        <input
+          id="password"
+          className="input-password"
+          type={showPassword ? 'text' : 'password'}
+          label="Senha"
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <FontAwesomeIcon
+          icon={showPassword ? faEye : faEyeSlash}
+          className="eye-password"
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default PasswordInput;
