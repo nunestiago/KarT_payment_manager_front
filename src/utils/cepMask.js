6dbcbff658545d30a6b2211527e0cb4328b686a7
Tiago Nunes <ntiagon@gmail.com>
2021-09-30 21:33:17 -0300

    fix cepmask algo

--- src/utils/cepMask.js
+++ src/utils/cepMask.js
@@ -4,7 +4,7 @@ function cepMask(e) {
   if (value.length <= 8) {
     value = value
       .replace(/\D/gu, '')
-      .replace(/(?<frist>\d{3})(?<second>\d{1,2})/u, '$1-$2');
+      .replace(/(?<frist>\d{5})(?<second>\d{1,2})/u, '$1-$2');
 
     e.currentTarget.value = value;
 
