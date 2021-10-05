3d3c77adb8aad5c4200cd08ebfe69c1f889b40a5
Karine Mendes <karinemendessantos@hotmail.com>
2021-10-05 19:08:17 -0300

    add modal editClient

---
+++ src/pages/EditClient/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ModalEditClient from '../../components/ModalEditClient';
+
+function EditClient() {
+  return(
+    <div>
+      <ModalEditClient />      
+    </div>
+  )
+}
+
+export default EditClient;
