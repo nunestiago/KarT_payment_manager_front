b6aaa404ea1e7dd858a3f8c9e26ab6d83afefe34
Karine Mendes <karinemendessantos@hotmail.com>
2021-10-04 21:58:41 -0300

    add cpf limit to 14 caracteres

--- src/utils/cpfMask.js
+++ src/utils/cpfMask.js
@@ -1,5 +1,25 @@
+import { toast } from 'react-toastify';
+  
+
 function cpfMask(e) {
+
+  const toastOpts = {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  };
+
   let { value } = e.currentTarget;
+
+  if (value.length > 14) {
+    toast.error('O CPF deve possuir apenas 14 números', toastOpts);
+    return false;
+  }
+  
 
   if (value.length <= 14) {
     value = value
