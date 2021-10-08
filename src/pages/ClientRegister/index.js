f5d7c420ca1ac3300e2ed2ac5a90719f1a51851d
Tiago Nunes <ntiagon@gmail.com>
2021-10-08 18:03:35 -0300

    new class to fix

--- src/pages/ClientRegister/index.js
+++ src/pages/ClientRegister/index.js
@@ -96,7 +96,7 @@ function ClientRegister() {
             {...register('email', { required: true })}
           />{' '}
           <div className="half">
-            <div>
+            <div className="addClient">
               <label htmlFor="cpf">CPF</label>
               <input
                 id="cpf"
@@ -106,7 +106,7 @@ function ClientRegister() {
                 onChange={cpfMask}
               />{' '}
             </div>
-            <div>
+            <div className="addClient">
               <label htmlFor="telefone">Telefone</label>
               <input
                 id="telefone"
@@ -118,7 +118,7 @@ function ClientRegister() {
             </div>
           </div>
           <div className="half">
-            <div>
+            <div className="addClient">
               <label htmlFor="cep">CEP</label>
               <input
                 id="cep"
@@ -131,7 +131,7 @@ function ClientRegister() {
                 }}
               />
             </div>
-            <div>
+            <div className="addClient">
               <label htmlFor="logradouro">Logradouro</label>
               <input
                 id="logradouro"
@@ -143,7 +143,7 @@ function ClientRegister() {
             </div>
           </div>
           <div className="half">
-            <div>
+            <div className="addClient">
               <label htmlFor="bairro">Bairro</label>
               <input
                 id="bairro"
@@ -153,7 +153,7 @@ function ClientRegister() {
                 onChange={e => setAddress({ bairro: e.target.value })}
               />{' '}
             </div>
-            <div>
+            <div className="addClient">
               <label htmlFor="cidade">Cidade</label>
               <input
                 id="cidade"
@@ -165,7 +165,7 @@ function ClientRegister() {
             </div>
           </div>
           <div className="half">
-            <div>
+            <div className="addClient">
               <label htmlFor="complemento">Complemento</label>
               <input
                 id="complemento"
@@ -175,7 +175,7 @@ function ClientRegister() {
                 onChange={e => setAddress({ complemento: e.target.value })}
               />
             </div>
-            <div>
+            <div className="addClient">
               <label htmlFor="pref">Ponto de Referência</label>
               <input id="pref" type="text" {...register('ponto_referencia')} />
             </div>
